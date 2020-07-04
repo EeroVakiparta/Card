6316f74ce79636f9098bca4053395043877fa80c
EeroVakiparta <eero.vakiparta@student.samk.fi>
2020-07-04 23:51:23 +0300

    First try with Postgres

--- src/main/java/com/arue/card/model/Card.java
+++ src/main/java/com/arue/card/model/Card.java
@@ -1,18 +1,29 @@
 package com.arue.card.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
+@Entity
 public class Card {
 
-    private final UUID id;
+    @Id
+    @GeneratedValue
+    private UUID id;
 
     @NotBlank
-    private final String name;
+    @Column
+    private String name;
+
+    public Card() {
+    }
 
     public Card(@JsonProperty("id") UUID id,
-                  @JsonProperty("name") String name) {
+                @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
@@ -21,7 +32,15 @@ public class Card {
         return id;
     }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
