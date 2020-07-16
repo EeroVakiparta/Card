00cdf682796e5c51bb2f786bb0ed8f7ab87c81ab
EeroVakiparta <eero.vakiparta@student.samk.fi>
2020-07-17 00:04:59 +0300

    Comments and war -> jar

---
+++ src/test/java/com/arue/card/controller/CardControllerTest.java
@@ -0,0 +1,12 @@
+package com.arue.card.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardControllerTest {
+
+    @Test
+    void create() {
+    }
+}
