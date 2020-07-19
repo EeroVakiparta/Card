766715ffb9cf29cd9950f2fbedcf77485acfa7d6
EeroVakiparta <eero.vakiparta@student.samk.fi>
2020-07-19 12:54:25 +0300

    API doc info

--- src/main/java/com/arue/card/config/SpringFoxConfig.java
+++ src/main/java/com/arue/card/config/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.arue.card.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +20,16 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("EbinCardGame API")
+                .version("1.0")
+                .description("API for managing cards")
+                .contact(new Contact("Ebin Crew", "http://www.ebincardgame.com", "n@gmail.com"))
                 .build();
     }
 }
